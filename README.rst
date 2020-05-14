fa76b2507ca99de4bbbde6b691018d01369ce5ef
Tomasz Knapik <tomasz.knapik@torchbox.com>
2020-05-14 17:25:22 +0100

    Add Front Door support

--- README.rst
+++ README.rst
@@ -1,11 +1,15 @@
 wagtail-azure-cdn
 =================
 
-An Azure CDN backend for Wagtail's
+An Azure CDN or Front Door backend for Wagtail's
 `front-end cache invalidator <https://docs.wagtail.io/en/latest/reference/contrib/frontendcache.html>`_.
 
 This module requires `azure-mgmt-cdn
-<https://pypi.org/project/azure-mgmt-cdn/>`_ as minimum to work properly.
+<https://pypi.org/project/azure-mgmt-cdn/>`_ as minimum to work properly with Azure CDN.
+
+
+This module requires `azure-mgmt-frontdoor
+<https://pypi.org/project/azure-mgmt-frontdoor/>`_ as minimum to work properly with Azure Front Door.
 
 
 Installation
@@ -15,9 +19,27 @@ Installation
 
    pip install wagtail-azure-cdn
 
+You can also install with dependencies. For Azure CDN support:
 
-Configuration
--------------
+.. code:: shell
+
+   pip install wagtail-azure-cdn[cdn]
+
+Or for Front Door support:
+
+.. code:: shell
+
+   pip install wagtail-azure-cdn[frontdoor]
+
+Or for both:
+
+.. code:: shell
+
+   pip install wagtail-azure-cdn[cdn,frontdoor]
+
+
+Configuration for Azure CDN
+---------------------------
 
 Add the following settings to your Wagtail project.
 
@@ -93,6 +115,58 @@ E.g. you can do this:
                 "jobs.torchbox.com": {
                     "CDN_ENDPOINT_NAME": "Your endpoint name",
                     "CREDENTIALS": get_azure_cli_credentials()[0]
+                },
+            },
+        },
+    }
+
+
+Configuration for Azure Front Door
+----------------------------------
+
+Add the following settings to your Wagtail project.
+
+.. code:: python
+
+    from azure.common.credentials import get_azure_cli_credentials
+
+    WAGTAILFRONTENDCACHE = {
+        "default": {
+            "BACKEND": "wagtail_azure_cdn.backends.AzureFrontDoorBackend",
+            "RESOURCE_GROUP_NAME": "Your resource group name",
+            "FRONTDOOR_NAME": "Your Front Door name",
+            "SUBSCRIPTION_ID": "Your subscription ID",
+            "CREDENTIALS": get_azure_cli_credentials()[0]
+        }
+    }
+
+
+* ``RESOURCE_GROUP_NAME``, ``FRONTDOOR_NAME`` and ``SUBSCRIPTION_ID`` can be all
+  obtained from the Azure portal.
+* ``CREDENTIALS`` is Azure credentials objects. They may be a callable to. `See
+  the documentation
+  <https://docs.microsoft.com/en-us/azure/python/python-sdk-azure-authenticate>`_
+  for the details.
+
+Multi-site configuration
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+The multi-site configuration works the same for the Front Door, just use `SITES`.
+
+
+.. code:: python
+
+    from azure.common.credentials import get_azure_cli_credentials
+
+    WAGTAILFRONTENDCACHE = {
+        "default": {
+            "BACKEND": "wagtail_azure_cdn.backends.AzureFrontDoorBackend",
+            "SITES": {
+                "torchbox.com": {
+                    "RESOURCE_GROUP_NAME": "Your resource group name",
+                    "FRONTDOOR_NAME": "Your Front Door name",
+                    "SUBSCRIPTION_ID": "Your subscription ID",
+                    "CREDENTIALS": get_azure_cli_credentials()[0],
                 },
             },
         },
--- wagtail_azure_cdn/tests/test_backends.py
+++ wagtail_azure_cdn/tests/test_backends.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 from faker import Faker
 
-from wagtail_azure_cdn.backends import AzureCdnBackend
+from wagtail_azure_cdn.backends import AzureCdnBackend, AzureFrontDoorBackend
 
 
 class TestAzureCdnBackend(test.TestCase):
@@ -146,7 +146,142 @@ class TestAzureCdnBackend(test.TestCase):
         )
         with self.assertRaisesRegex(
             ImproperlyConfigured,
-            "Missing Azure CDN front-end cache invalidator for setting: "
+            "Missing Azure front-end cache invalidator for setting: "
+            r"resource_group_name \(www.oxfam.org.uk\)",
+        ):
+            backend._get_setting_for_hostname("www.oxfam.org.uk", "resource_group_name")
+
+    def test_filter_urls_by_hostname(self):
+        backend = AzureCdnBackend({})
+        fake = Faker()
+        # Faker may return repeated URLs so make it a set.
+        fake_urls = frozenset(fake.uri() for _ in range(2000))
+        filtered_urls = backend._filter_urls_by_hostname(fake_urls)
+        self.assertEqual(
+            len([item for sublist in filtered_urls.values() for item in sublist]),
+            len(fake_urls),
+        )
+
+
+class TestAzureFrontDoorBackend(test.TestCase):
+    def test_subscription_id_setting(self):
+        backend = AzureFrontDoorBackend({"SUBSCRIPTION_ID": "test-subscription-id"})
+        self.assertEqual(
+            backend._get_setting_for_hostname("torchbox.com", "subscription_id"),
+            "test-subscription-id",
+        )
+
+    def test_resource_group_name(self):
+        backend = AzureFrontDoorBackend({"RESOURCE_GROUP_NAME": "some-resource"})
+        self.assertEqual(
+            backend._get_setting_for_hostname("torchbox.com", "resource_group_name"),
+            "some-resource",
+        )
+
+    def test_cdn_profile_name(self):
+        backend = AzureFrontDoorBackend({"FRONTDOOR_NAME": "front-door-1"})
+        self.assertEqual(
+            backend._get_setting_for_hostname("torchbox.com", "frontdoor_name"),
+            "front-door-1",
+        )
+
+    def test_frontdoor_service_url(self):
+        backend = AzureFrontDoorBackend(
+            {"FRONTDOOR_SERVICE_URL": "https://torchbox.com"}
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname("torchbox.com", "frontdoor_service_url"),
+            "https://torchbox.com",
+        )
+
+    def test_multi_site(self):
+        backend = AzureFrontDoorBackend(
+            {
+                "RESOURCE_GROUP_NAME": "oxfam-production",
+                "SUBSCRIPTION_ID": "622eced0-8196-4da1-a370-70dc87be5596",
+                "SITES": {
+                    "www.oxfam.org.uk": {"FRONTDOOR_NAME": "oxfam"},
+                    "volunteer.oxfam.org.uk": {"FRONTDOOR_NAME": "volunteering"},
+                },
+            }
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname("www.oxfam.org.uk", "frontdoor_name"),
+            "oxfam",
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "www.oxfam.org.uk", "resource_group_name"
+            ),
+            "oxfam-production",
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname("www.oxfam.org.uk", "subscription_id"),
+            "622eced0-8196-4da1-a370-70dc87be5596",
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "volunteer.oxfam.org.uk", "subscription_id"
+            ),
+            "622eced0-8196-4da1-a370-70dc87be5596",
+        )
+
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "volunteer.oxfam.org.uk", "frontdoor_name"
+            ),
+            "volunteering",
+        )
+
+    def test_multi_site_inheritance(self):
+        backend = AzureFrontDoorBackend(
+            {
+                "RESOURCE_GROUP_NAME": "oxfam-production",
+                "SUBSCRIPTION_ID": "622eced0-8196-4da1-a370-70dc87be5596",
+                "SITES": {
+                    "www.oxfam.org.uk": {"FRONTDOOR_NAME": "oxfam"},
+                    "volunteer.oxfam.org.uk": {
+                        "RESOURCE_GROUP_NAME": "oxfam-volunteering",
+                        "FRONTDOOR_NAME": "volunteering",
+                    },
+                },
+            }
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "www.oxfam.org.uk", "resource_group_name"
+            ),
+            "oxfam-production",
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "volunteer.oxfam.org.uk", "resource_group_name"
+            ),
+            "oxfam-volunteering",
+        )
+
+    def test_multi_site_missing_setting_raises(self):
+        backend = AzureFrontDoorBackend(
+            {
+                "SUBSCRIPTION_ID": "622eced0-8196-4da1-a370-70dc87be5596",
+                "SITES": {
+                    "www.oxfam.org.uk": {"FRONTDOOR_NAME": "oxfam"},
+                    "volunteer.oxfam.org.uk": {
+                        "RESOURCE_GROUP_NAME": "oxfam-volunteering",
+                        "FRONTDOOR_NAME": "volunteering",
+                    },
+                },
+            }
+        )
+        self.assertEqual(
+            backend._get_setting_for_hostname(
+                "volunteer.oxfam.org.uk", "resource_group_name"
+            ),
+            "oxfam-volunteering",
+        )
+        with self.assertRaisesRegex(
+            ImproperlyConfigured,
+            "Missing Azure front-end cache invalidator for setting: "
             r"resource_group_name \(www.oxfam.org.uk\)",
         ):
             backend._get_setting_for_hostname("www.oxfam.org.uk", "resource_group_name")
